4657f31990090d4195851a9590e94358056e6460
AndreyKeleberda <a.keleberda@ukr.net>
2022-03-14 18:21:25 +0200

    fix mobile menu

--- src/js modules/mobile-menu.js
+++ src/js modules/mobile-menu.js
@@ -5,21 +5,15 @@
   const headerContainer = document.querySelector('.header');
 
   const toggleMenu = () => {
-    // const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    // openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
     headerContainer.classList.toggle('menu-is-open');
     closeMenuBtn.classList.toggle('menu-is-open');
     openMenuBtn.classList.toggle('menu-is-open');
-    // const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-    // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
